f1f0b9a5b44781e36d3e58f2cc36a248e717c856
Satheeswaran M <Sathees37081@gmail.com>
2026-06-15 19:50:07 +0530

    Refactor main function for encoding operations

--- test_encode.c
+++ test_encode.c
@@ -2,30 +2,31 @@
 #include "encode.h"
 #include "types.h"
 
-int main()
+int main(int argc, char *argv[])
 {
     EncodeInfo encInfo;
-    uint img_size;
+    int ret = check_operation_type(argv);
 
-    // Fill with sample filenames
-    encInfo.src_image_fname = "beautiful.bmp";
-    encInfo.secret_fname = "secret.txt";
-    encInfo.stego_image_fname = "stego_img.bmp";
+    if (ret == e_encode)
+    {   // encoding
+        int ret = read_and_validate_encode_args(argv, &encInfo);
 
-    // Test open_files
-    if (open_files(&encInfo) == e_failure)
-    {
-    	printf("ERROR: %s function failed\n", "open_files" );
-    	return 1;
+        if (ret == e_failure)
+        {
+            printf("validation is wrong.\n");
+            return 0;
+        }
+
+        ret = do_encoding(&encInfo);
+        if (ret == e_failure)
+        {
+            printf("encoding is failed\n");
+            return 0;
+        }
+
+        printf("encoding is successful ... \n");
+        return 0;
     }
-    else
-    {
-    	printf("SUCCESS: %s function completed\n", "open_files" );
-    }
-
-    // Test get_image_size_for_bmp
-    img_size = get_image_size_for_bmp(encInfo.fptr_src_image);
-    printf("INFO: Image size = %u\n", img_size);
 
     return 0;
 }
