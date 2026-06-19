5d698ce43b98e6c19e6213b9750a6efe45aab526
Satheeswaran M <Sathees37081@gmail.com>
2026-06-19 12:34:59 +0530

    Add function prototypes for encoding pipeline

--- encode.h
+++ encode.h
@@ -1,3 +1,6 @@
+/* File: encode.h
+ * Brief: EncodeInfo struct and function prototypes for the encoding pipeline. */
+
 #ifndef ENCODE_H
 #define ENCODE_H
 
@@ -67,6 +70,9 @@ Status copy_bmp_header(FILE *fptr_src_image, FILE *fptr_dest_image);
 /* Store Magic String */
 Status encode_magic_string(const char *magic_string, EncodeInfo *encInfo);
 
+/* Encode secret file extension size */
+Status encode_secret_file_extn_size(int extn_size, EncodeInfo *encInfo);
+
 /* Encode secret file extenstion */
 Status encode_secret_file_extn(const char *file_extn, EncodeInfo *encInfo);
 
@@ -81,6 +87,9 @@ Status encode_data_to_image(char *data, int size, FILE *fptr_src_image, FILE *fp
 
 /* Encode a byte into LSB of image data array */
 Status encode_byte_to_lsb(char data, char *image_buffer);
+
+/* Encode a size into LSB of image data array */
+Status encode_size_to_lsb(int data, char *image_buffer);
 
 /* Copy remaining image bytes from src to stego image after encoding */
 Status copy_remaining_img_data(FILE *fptr_src, FILE *fptr_dest);
