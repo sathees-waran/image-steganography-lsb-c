ac14a53aec8fa914eff897694048ae14adb2c9d0
Satheeswaran M <Sathees37081@gmail.com>
2026-06-19 12:34:24 +0530

    Add header comments to common.h
    Added file header comments for better documentation.

--- common.h
+++ common.h
@@ -1,7 +1,11 @@
+/* File: common.h
+ * Brief: Shared constants (magic string) used by both encoder and decoder. */
+ 
 #ifndef COMMON_H
 #define COMMON_H
-
+ 
 /* Magic string to identify whether stegged or not */
 #define MAGIC_STRING "#*"
-
+ 
 #endif
+ 
